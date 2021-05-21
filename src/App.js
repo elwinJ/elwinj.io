b2bc0cf5e108bc6e35145091042a37e6535001a3
elwinJ <elwinjosue@gmail.com>
2021-05-21 13:25:23 -0400

    Re-added homepage in package.json, also modified imports and code wrapers in App.js and Index.js to solve post-deployment issues.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
+import { Route, Link, Switch, Redirect } from "react-router-dom";
 import "./App.css"
 import NavBar from './Components/NavBar'
 import Home from './Components/Home'
@@ -11,40 +11,41 @@ import Contact from './Components/Contact'
 class App extends Component {
   render() {
     return (
-        <Router>
-              <div className="App">
-                
-              <NavBar/>
-                
-                <div className="app_content">
-                  <Switch>
-                    
-                    <Route exact path="/">
-                      <Redirect to="/home" />
-                    </Route>
 
-                    <Route path="/home">
-                      <Home/>
-                    </Route>
+      // <Router>
+      <div className="App">
+        
+        <NavBar/>
+        
+        <div className="app_content">
+          <Switch>
+            
+            <Route exact path="/">
+              <Redirect to="/home" />
+            </Route>
 
-                    <Route  path="/about">
-                      <About/>
-                    </Route>
+            <Route path="/home">
+              <Home/>
+            </Route>
 
-                    <Route  path="/projects">
-                      <Projects/>
-                    </Route>
+            <Route  path="/about">
+              <About/>
+            </Route>
 
-                    <Route  path="/contact">
-                      <Contact/>
-                    </Route>
+            <Route  path="/projects">
+              <Projects/>
+            </Route>
 
-                  </Switch>
-                  
-                </div>
-                
-              </div>
-        </Router>
+            <Route  path="/contact">
+              <Contact/>
+            </Route>
+
+          </Switch>
+          
+        </div>
+          
+      </div>
+      // </Router>
     );
   }
 }
